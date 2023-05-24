694e29be749ec40c3d7924bf421bf3a6b5526425
Streltsov Ivan <principdereva@gmail.com>
2023-05-24 12:39:14 +0200

    удалил ненужный вендорный код

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initTabs} from './vendor/init-tabs';
 
@@ -15,7 +14,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     initTabs();
     const form = new Form();
     window.form = form;
